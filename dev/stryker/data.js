a7e3686bb093b57cf80b48ed761ad06d6f02b8ab
github-action-benchmark <github@users.noreply.github.com>
2026-07-27 00:28:07 +0000

    add Mutation score (customBiggerIsBetter) benchmark result for c9e2a37a00fb1ae6885e306ecb0c18eb6b1ddcc1

---
+++ dev/stryker/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1785112086836,
+  "repoUrl": "https://github.com/Chris-Wolfgang/ETL-Test-Kit",
+  "entries": {
+    "Mutation score": [
+      {
+        "commit": {
+          "author": {
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com",
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c9e2a37a00fb1ae6885e306ecb0c18eb6b1ddcc1",
+          "message": "Merge pull request #246 from Chris-Wolfgang/vNext\n\nrelease: 0.10.1 (thorough-review maintenance tier + Abstractions 0.16.0)",
+          "timestamp": "2026-07-26T20:21:11-04:00",
+          "tree_id": "3caf71bb9e6a1ddde30b4276817ed698c4173ab1",
+          "url": "https://github.com/Chris-Wolfgang/ETL-Test-Kit/commit/c9e2a37a00fb1ae6885e306ecb0c18eb6b1ddcc1"
+        },
+        "date": 1785112084459,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Mutation score",
+            "value": 72.94,
+            "unit": "%"
+          }
+        ]
+      }
+    ]
+  }
+}
